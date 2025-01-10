7093ae1dfbd0efad4755b818a05caf7735e2be85
Zhouxing Shi <zhouxingshichn@gmail.com>
2025-01-09 23:02:33 -0800

    Add a copy of abcrown

---
+++ alpha-beta-CROWN/complete_verifier/models/nonlinear_NNs/models/__init__.py
@@ -0,0 +1,22 @@
+#########################################################################
+##   This file is part of the α,β-CROWN (alpha-beta-CROWN) verifier    ##
+##                                                                     ##
+##   Copyright (C) 2021-2025 The α,β-CROWN Team                        ##
+##   Primary contacts: Huan Zhang <huan@huan-zhang.com> (UIUC)         ##
+##                     Zhouxing Shi <zshi@cs.ucla.edu> (UCLA)          ##
+##                     Xiangru Zhong <xiangru4@illinois.edu> (UIUC)    ##
+##                                                                     ##
+##    See CONTRIBUTORS for all author contacts and affiliations.       ##
+##                                                                     ##
+##     This program is licensed under the BSD 3-Clause License,        ##
+##        contained in the LICENCE file in this directory.             ##
+##                                                                     ##
+#########################################################################
+from models.vit import *
+
+Models = {
+    'vit_1_mnist': ViT_1layer_mnist,
+    'vit_1_cifar': ViT_1layer_cifar,
+    'vit_2_cifar': ViT_2layer_cifar,
+    'vit_2_mnist': ViT_2layer_mnist,
+}
